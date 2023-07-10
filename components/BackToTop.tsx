3f7326d231e079d9c07c038b2c728ba36268af88
Dennis Sebalemba <dennis.sebalemba@gmail.com>
2023-07-10 05:05:54 +0300

    Update `BackToTop.tsx`

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -28,7 +28,7 @@ export default function BackToTop({}: Props) {
     <button
       className="nx-text-xs flex flex-row gap-2 nx-font-medium nx-text-gray-500 hover:nx-text-gray-900 dark:nx-text-gray-400 dark:hover:nx-text-gray-100 contrast-more:nx-text-gray-800 contrast-more:dark:nx-text-gray-50"
       onClick={scrollToTop}
-      style={{ display: visible ? 'inline' : 'none' }}
+      style={{ display: visible }}
       children={
         <>
           <ArrowUpCircle size={20} /> Back to top
